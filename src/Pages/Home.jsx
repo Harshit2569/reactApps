04fe665b1d76a418623874e4fa4c1f24b0757d18
Harshit <cr273916@gmail.com>
2025-11-13 22:53:39 +0530

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+// import React from 'react'
+
+// function Home() {
+//   return (
+//     <div>
+//       <h1>Hello world</h1>
+//     </div>
+//   )
+// }
+
+// export default Home
+
+import React from "react";
 
 function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <div id="nav">
+        <h2>Harshit</h2>
+        <div id="list">
+          <h4>About</h4>
+          <h4>Work</h4>
+          <h4>Career</h4>
+          <h4>Contact</h4>
+        </div>
+      </div>
+      <h1 className="name">Harshit Upadhyay</h1>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
